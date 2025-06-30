40f6b9811db6ed4e15e7781d2de9a20268a53b38
mSapik <sapik.max@yandex.ru>
2025-06-30 16:27:12 +0300

    Upd

--- esphome/components/mhi_zj/climate.py
+++ esphome/components/mhi_zj/climate.py
@@ -1,7 +1,6 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import climate_ir
-from esphome.const import CONF_ID
 
 CODEOWNERS = ["@mSapik"]
 AUTO_LOAD = ['climate_ir']
@@ -9,12 +8,8 @@ AUTO_LOAD = ['climate_ir']
 mhi_ns = cg.esphome_ns.namespace('mhi_zj')
 MhiClimate = mhi_ns.class_('MhiClimate', climate_ir.ClimateIR)
 
-#CONFIG_SCHEMA = climate_ir.CLIMATE_IR_SCHEMA.extend({
-CONFIG_SCHEMA = climate_ir.climate_ir_with_receiver_schema(MhiClimate).extend({
-    cv.GenerateID(): cv.declare_id(MhiClimate),
-})
+CONFIG_SCHEMA = climate_ir.climate_ir_with_receiver_schema(MhiClimate)
 
-
-def to_code(config):
-    var = cg.new_Pvariable(config[CONF_ID])
-    yield climate_ir.register_climate_ir(var, config)
+async def to_code(config):
+    var = await climate_ir.new_climate_ir(config)
+    await cg.register_component(var, config)
