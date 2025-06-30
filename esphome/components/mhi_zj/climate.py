2bc377b6af9435de75426e62af84c4ae5301115f
mSapik <sapik.max@yandex.ru>
2025-06-30 16:33:10 +0300

    upd

--- esphome/components/mhi_zj/climate.py
+++ esphome/components/mhi_zj/climate.py
@@ -12,4 +12,3 @@ CONFIG_SCHEMA = climate_ir.climate_ir_with_receiver_schema(MhiClimate)
 
 async def to_code(config):
     var = await climate_ir.new_climate_ir(config)
-    await cg.register_component(var, config)
